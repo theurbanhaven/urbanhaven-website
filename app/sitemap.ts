5fe878775c589a5c2327e4a91127709e5670615a
UrbanHaven <tech@theurbanhaven.in>
2026-07-23 10:58:38 +0530

    add_baseurl

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = "https://theurbanhaven.in"
+    const baseUrl = "https://www.theurbanhaven.in";
     return [
         {
             url: `${baseUrl}`,
